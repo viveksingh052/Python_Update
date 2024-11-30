cd3e2f345ee151c9bb3c8359b6558a41cfa7a345
vivek singh <vs292713@gmail.com>
2024-11-30 19:10:18 +0530

    very basic calculator

---
+++ Day_01/making_calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+# very basic calculators for doing computations of two numbers
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
