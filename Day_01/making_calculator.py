4bc27924a87505db202733aaa502418ea3e979ca
vivek singh <vs292713@gmail.com>
2024-12-01 12:37:58 +0530

    Day 02 commit

--- Day_01/making_calculator.py
+++ Day_01/making_calculator.py
@@ -4,8 +4,13 @@ b = int(input("Enter the second number:"))
 # very basic calculators for doing computations of two numbers
 
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
+print("The value of addition is:",a+b)
+print("The value of subtraction is:",a-b)
+print("The value of multiplication is:",a*b)
+print("The value of division is:",a/b)
+print("The value of modulus is:",a%b)
+print("The value of floor division is:",a//b)
+
+# Division (/): Returns a floating-point result (with decimal values).
+# Modulus (%): Returns the remainder of the division.
+# Floor Division (//): Returns the quotient, rounded down to the nearest integer.
