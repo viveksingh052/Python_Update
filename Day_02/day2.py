14e15463a5930c5b732581396abcb0a7fa78e328
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-12-01 12:42:43 +0530

    Add files via upload

---
+++ Day_02/day2.py
@@ -0,0 +1,27 @@
+#Strings 
+
+name = "vivek"
+# print(name[0])
+# print(name[1])
+
+# for char in name:
+#     print(char)
+
+#String slicing
+print(name[0:5])
+print(name[:]) #it takes the length of the string 
+print(name[0:-3])  # it takes the length of the string print(name[0:len(name)-3]) = 2
+print(name[-3:-1]) # it takes the length of the string print(name[len(name)-3]: len(name)-1)] = print(name[2:4])
+
+
+#Length of the string
+print(len(name))
+
+alphabets = "VIVEK"
+
+for i in alphabets:
+    print(i)
+
+nm = "Harry"
+print(nm[-4:-2]) #it prints ar 
+
