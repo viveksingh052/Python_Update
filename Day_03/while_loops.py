a6e9d303e00d9ca704dbd27a160dec1dbd343396
vivek singh <vs292713@gmail.com>
2024-12-02 18:01:18 +0530

    While Loops

---
+++ Day_03/while_loops.py
@@ -0,0 +1,16 @@
+# count = 5
+
+# while(count>0):
+#     print(count)
+#     count = count-1
+
+# We can even use the else statement with the while loop
+
+# count = 5
+
+# while(count>0):
+#     print(count)
+#     count-=1
+# else:
+#     print("Loop will be terminated")
+
