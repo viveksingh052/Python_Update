009d8e056f5ffca7d3986cc19a7b8ece6e603605
vivek singh <vs292713@gmail.com>
2024-12-02 18:49:49 +0530

    Additional Codes for the day 3

---
+++ Day_03/additional.py
@@ -0,0 +1,20 @@
+colors = ["red","green","blue","pink","black"]
+
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+    print("*"*10)
+
+count = -5
+
+while(count>0):
+    print(count)
+
+    count = count -1
+else:
+    print("Program enter in the while loop")
+
+# In do while loop the loop body will be executed whether the codition is true or false . After
+# the execution of loop body if the codition will be true(checking while(condition)) then the next iteration of the loop
+# is executed else not executed
