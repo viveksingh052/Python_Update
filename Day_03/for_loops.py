ca4eca4be1308df59c6dcb1db5ec8827288ac615
vivek singh <vs292713@gmail.com>
2024-12-02 17:45:27 +0530

    For Loops

---
+++ Day_03/for_loops.py
@@ -0,0 +1,21 @@
+# name = "vivek"
+
+# for i in name:
+#     print(i , end =",")
+
+
+# color = ["red","blue","pink","green"]
+
+# for i in color:
+#     print(i)
+
+# for k in range(5):
+#     print(k)
+
+# for k in range(4,9):
+#     print(k)
+
+# Explore about third parameter of range (ie range(x, y, z))
+
+# for k in range(2,10,2):
+    print(k)
