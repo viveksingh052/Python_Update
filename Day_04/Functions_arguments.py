bf506bfeef5a5b7d55273b5ecf73651c1d4d5387
vivek singh <vs292713@gmail.com>
2024-12-03 23:39:40 +0530

    Functions Arguments

---
+++ Day_04/Functions_arguments.py
@@ -0,0 +1,50 @@
+#Functions arguments
+
+# 1. DEFAULT Arguments
+
+# def average(a,b):
+#     print("The average is ",(a+b)/2)
+
+# average(3,4)
+
+# def average(a=6,b=6):
+#     print("The average is ",(a+b)/2)
+
+# average()
+
+# def average(a=5,b=5):
+#     print("The average is ",(a+b)/2)
+
+# average(b=20)
+
+
+# 2.Keyword Argument
+
+# def name(fname, mname, lname):
+#     print("Hello,", fname, mname, lname)
+
+# name(mname = "Peter", lname = "Wesker", fname = "Jade")
+
+# Hello, Jade Peter Wesker
+
+
+# 3.Required Arguments
+
+# def name(fname, mname, lname):
+#     print("Hello,", fname, mname, lname)
+
+# name("Peter", "Quill")
+
+
+# name("Peter", "Quill")\
+# TypeError: name() missing 1 required positional argument: 'lname'
+
+# 4.Variable Length Arguments
+
+# a.Arbitrary Arguments:
+
+def name(*name):
+    print("Hello",name[0],name[1],name[2])
+
+name("Vivek","Aman","Shobhit")
+
