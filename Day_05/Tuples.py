100c5462ae51406b397bd6cc4d92f86273eb2ad2
vivek singh <vs292713@gmail.com>
2024-12-04 20:18:58 +0530

    Tuples.py

---
+++ Day_05/Tuples.py
@@ -0,0 +1,27 @@
+# tuple1 = (1,2,2,3,5,4,6)
+# tuple2 = ("Red", "Green", "Blue")
+# print(tuple1)
+# print(tuple2)
+
+# country = ("Spain", "Italy", "India",)
+# #            [0]      [1]      [2]     
+# print(country[0])
+# print(country[1])
+# print(country[2])
+
+
+# country = ("Spain", "Italy", "India", "England", "Germany")
+# #            [0]      [1]      [2]       [3]        [4]
+# print(country[-1]) # Similar to print(country[len(country) - 1])
+# print(country[-3])
+# print(country[-4])
+
+
+#  Check for item:
+# We can check if a given item is present in the tuple. This is done using the in keyword.
+
+# country = ("Spain", "Italy", "India", "England", "Germany")
+# if "Germany" in country:
+#     print("Germany is present.")
+# else:
+#     print("Germany is absent.")
