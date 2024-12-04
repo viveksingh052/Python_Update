b442e0e2f0761035f438ef1b0b9541e4268d7421
vivek singh <vs292713@gmail.com>
2024-12-04 14:59:02 +0530

    List Python File

---
+++ Day_05/List.py
@@ -0,0 +1,20 @@
+# lst1 = [1,2,2,3,5,4,6]
+# lst2 = ["Red", "Green", "Blue"]
+# print(lst1)
+# print(lst2)
+
+# details = ["Abhijeet", 18, "FYBScIT", 9.8]
+# print(details)
+
+
+# colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+ 
+# if "Maroon" in colors:
+#     print("Color is present in the given color list")
+# else:
+#     print("Color is not present!")
+
+# animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+
+# print(animals[0:8])
+# print(animals[-7:-2])	#using negative indexes'
