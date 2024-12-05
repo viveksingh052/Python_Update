f2150ae231aec802c31a8fa6de543390fd2b9da8
vivek singh <vs292713@gmail.com>
2024-12-05 13:00:31 +0530

    Doc Strings

---
+++ Day_06/doc_Strings.py
@@ -0,0 +1,11 @@
+
+def square(n):
+    '''This functions gives you the square of the 
+    number.
+    '''
+    print(n**2)
+
+square(5)
+print(square.__doc__)  #This is the docstring
+
+# Python docstrings are the string literals that appear right after the definition of a function, method, class, or module
