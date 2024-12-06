65e5908daca066db4f7eb61913f666b8b21ccf92
vivek singh <vs292713@gmail.com>
2024-12-06 23:52:19 +0530

    For Loops With Else In Python

---
+++ Day_07/for_loop_with_else.py
@@ -0,0 +1,28 @@
+name = "vivek"
+
+for char in name:
+    print(char)
+
+else:
+    print("All the values are printed")
+
+i = 0
+
+while(i<5):
+    print(i)
+    i +=1
+
+else:
+    print("All the values of the i are printed")
+
+
+m = 0
+while(m<7):
+    print(m)
+    m = m+1
+
+    if(m==4):
+     break
+
+else:
+    print("no")
