85fa4c47ed34bc925542abf401bd23d92118e418
vivek singh <vs292713@gmail.com>
2024-12-07 15:41:08 +0530

    Raising Custom Eroors

---
+++ Day_08/Raising_custom_eroors.py
@@ -0,0 +1,11 @@
+# a = int(input("Enter the number which you want:"))
+
+# if a<5 or a>9 :
+#     raise ValueError("The number is not ranging between 5 and 9")
+
+
+b = input("Enter the string: ")
+if(b!= "quit"):
+    raise ValueError("Input!= \'quit\'")
+else:
+    print("Program is running smoothly!")
