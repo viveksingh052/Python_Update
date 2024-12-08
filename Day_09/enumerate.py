26d220bc7b33474396d7fdd26d910ee4edb9a07f
vivek singh <vs292713@gmail.com>
2024-12-08 19:53:47 +0530

    Enumerate.py

---
+++ Day_09/enumerate.py
@@ -0,0 +1,17 @@
+# marks = [45,22,33,87,90,11]
+
+# for index, mark in enumerate(marks):
+#     print(f"This is index{index} and This is marks {mark} .")
+#     if(index==4):
+#         print(f"At this index {index} This is marks of the vivek")
+
+
+# marks = [45,22,33,87,90,11]
+
+# for index, mark in enumerate(marks , start=2):
+#     print(f"This is index{index} and Marks = {mark} .")
+#     if(index==4):
+#         print(f"At this index {index} This is marks of the vivek")
+
+# The enumerate function is often used when you need to loop over a sequence
+# and perform some action with both the index and value of each element
