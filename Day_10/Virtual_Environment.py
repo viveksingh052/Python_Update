b1328f42c139d527b520f367b8689bcac4791315
vivek singh <vs292713@gmail.com>
2024-12-09 13:35:55 +0530

    Virtual Environment

---
+++ Day_10/Virtual_Environment.py
@@ -0,0 +1,12 @@
+# 1.To create a virtual Environment
+# python -m venv myenv
+
+# 2.To Deactivate the virtual Environment
+# deactivate
+
+# 3.Create the Requirement.txt File
+# pip install -r requirements.txt
+
+# 4.Output the list of install packages
+# pip freeze >requirements.txt
+
