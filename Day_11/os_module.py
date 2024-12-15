9fa7b951b229835bf03a408e9d0bdec258264941
vivek singh <vs292713@gmail.com>
2024-12-15 20:39:02 +0530

    os module

---
+++ Day_11/os_module.py
@@ -0,0 +1,30 @@
+# import os
+
+# if(not os.path.exists("data1")):
+#     os.mkdir("data1")
+
+# # os.mkdir("data1")
+
+# for i in range(12,18):
+#     os.mkdir(f"data{i+1}")
+
+import os 
+
+# for i in range(0,9):
+#     os.rename(f"Tutorial_0{i+1}",f"Day_0{i+1}")
+
+# for i in range(9,11):
+    # os.rename(f"Tutorial{i+1}",f"Day_{i+1}")
+
+folder = os.getcwd()
+print(folder)
+
+# folders = os.listdir("c:/Users/vs292/OneDrive/Desktop/20DAY PYTHON")
+
+# for folder in folders:
+#     print(folder))
+# print(folders) 
+
+# for folder in folders:
+#     print(folder)
+
